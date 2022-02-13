ff5629a6db9f7b83d3184d598ecc66c606d2a23f
saneftw <39649316+saneftw@users.noreply.github.com>
2022-02-13 22:39:34 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -15,7 +15,7 @@ static const char col_cyan[]        = "#ff1493";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  #08e8de  },
 };
 
 /* tagging */
