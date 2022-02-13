e06c9771e72cbaac90a05c9768792a86926111cc
saneftw <39649316+saneftw@users.noreply.github.com>
2022-02-13 21:46:42 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -70,7 +70,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,			XK_w,		spawn,	   SHCMD("$BROWSER") },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
