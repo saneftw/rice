1e612393a6bfef8e950b2c2cdf9a991692c70dbb
saneftw <39649316+saneftw@users.noreply.github.com>
2022-02-15 19:09:03 +0200

    Update config.h

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=11:antialias=true:autohint=true";
+static char *font = "Monospace:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 3;
 
 /*
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /*alpha */
-float alpha = 0.7;
+float alpha = 0.6;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
