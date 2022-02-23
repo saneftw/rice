8f04794f4e6b7fc52536d2f79550febf2d32fb7d
saneftw <39649316+saneftw@users.noreply.github.com>
2022-02-23 21:33:16 +0200

    Update blocks.h

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-      {"|", "/opt/dwmblocks/scripts/kernel",		    360,		   2},
+      {"|🐧", "/opt/dwmblocks/scripts/kernel",		    360,		   2},
   
-    	{"", "/opt/dwmblocks/scripts/weather",		6,			1},
+    	{"🪟", "/opt/dwmblocks/scripts/weather",		6,			1},
 
-    	{"", "/opt/dwmblocks/scripts/btc",			6,			1},
+    	{"💰", "/opt/dwmblocks/scripts/btc",			6,			1},
 
     	{"", "/opt/dwmblocks/scripts/cpu",			6,			1},
    	
-    	{"", "/opt/dwmblocks/scripts/pacupdate",		360,		        9},
+    	{"📦", "/opt/dwmblocks/scripts/pacupdate",		360,		        9},
 	
 	    {"", "/opt/dwmblocks/scripts/memory",	        6,		            1},
 
-	    {"  ", "/opt/dwmblocks/scripts/clock",			5,		            0},
+	    {" 🕒 ", "/opt/dwmblocks/scripts/clock",			5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
